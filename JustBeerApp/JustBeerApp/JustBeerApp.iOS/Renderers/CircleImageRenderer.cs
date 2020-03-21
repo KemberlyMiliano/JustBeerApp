bdec0c805be81907fd8388513d7849d70b665117
Zekaos <45668362+Zekaos@users.noreply.github.com>
2020-03-21 15:08:16 -0400

    Custom Renderer & BeerInfo UI

---
+++ JustBeerApp/JustBeerApp/JustBeerApp.iOS/Renderers/CircleImageRenderer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using JustBeerApp.Controls;
+using JustBeerApp.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CircleImage), typeof(CircleImageRenderer))]
+namespace JustBeerApp.iOS.Renderers
+{
+    public class CircleImageRenderer : ImageRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement != null || Element == null) return;
+            CreateCircle();
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (e.PropertyName == VisualElement.HeightProperty.PropertyName || e.PropertyName == VisualElement.WidthProperty.PropertyName)
+            {
+                CreateCircle();
+            }
+        }
+        private void CreateCircle()
+        {
+            try
+            {
+                double min = Math.Min(Element.Width, Element.Height);
+                Control.Layer.CornerRadius = (float)(min / 2.0);
+                Control.Layer.MasksToBounds = false;
+                Control.Layer.BorderColor = Color.White.ToCGColor();
+                Control.Layer.BorderWidth = 1;
+                Control.ClipsToBounds = true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Unable to create circle image: " + ex);
+            }
+        }
+    }
+}
