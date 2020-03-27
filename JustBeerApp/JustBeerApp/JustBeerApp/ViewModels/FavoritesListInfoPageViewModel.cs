d005674329d17e38384bba38f05b6b0a2665f8e1
DiegoJavierJ <49220228+DiegoJavierJ@users.noreply.github.com>
2020-03-27 00:43:41 -0400

    Update

--- JustBeerApp/JustBeerApp/JustBeerApp/ViewModels/FavoritesListInfoPageViewModel.cs
+++ JustBeerApp/JustBeerApp/JustBeerApp/ViewModels/FavoritesListInfoPageViewModel.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace JustBeerApp.ViewModels
 {
@@ -15,15 +17,18 @@ namespace JustBeerApp.ViewModels
         public ObservableCollection<Data> Data { get; set; }
         public bool IsRunning { get; set; }
         public string BeerId { get; set; }
+        public ICommand ShowData { get; set; }
         public FavoritesListInfoPageViewModel(INavigationService navigationService, IApiBeerService apiService, IPageDialogService pageDialogService) : base(navigationService, apiService, pageDialogService)
         {
-            GetBeerData2();
+            ShowData = new Command(async () =>   await GetBeerData2());
         }
         public async Task GetBeerData2()
         {
+            IsRunning = true;
             var result = await ApiManager.GetBeerAsync();
             if (result != null)
                 Data = new ObservableCollection<Data>(result);
+            IsRunning = false;
         }
     }
 }
--- JustBeerApp/JustBeerApp/JustBeerApp/Views/FavoritesListInfoPage.xaml.cs
+++ JustBeerApp/JustBeerApp/JustBeerApp/Views/FavoritesListInfoPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JustBeerApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
